dd6ebb92da64ea8b457ee058622f82f24e5c1f3e
otaviov <amandavv32@gmail.com>
2022-06-06 18:46:01 -0300

    continua o bug

--- controledevendas/src/classes/Dados.java
+++ controledevendas/src/classes/Dados.java
@@ -359,7 +359,7 @@ public class Dados {
                 senha = aux;
                 linha = linha.substring(pos + 1);
                
-                perfil = new Integer (linha);
+                perfil = 0;
                 
 
                
